0f8b0f0242a3b2b3646aaddb62830325dac0f0b7
Rhysa <rhysalee@hawaii.edu>
2020-02-13 09:56:40 -1000

    Initial Commit

---
+++ appBuildOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appBuildOne
+//
+//  Created by Rhysa Lee on 2/13/20.
+//  Copyright © 2020 Rhysa Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
